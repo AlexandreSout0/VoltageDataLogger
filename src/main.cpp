8e429048c92b829c2a5bd73d9505eaaec3de53cf
AlexandreSout0 <soutoinf@gmail.com>
2023-11-19 14:21:39 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -23,35 +23,52 @@ const char* password = "sorvetedequeijo";
 
 int bufferSize = 0; // Tamanho do buffer para armazenar os últimos valores
 float valores[1000];
-int alx = 0; // Índice atual no buffer
 
 #define R1 150000
 #define R2 750000
 
-int count = 0;
+const int numSamples = 10;  // Número de amostras para a média móvel
+int samples[numSamples];
+int indice = 0;
+
+int FilterMovingAverage(int inputValue) {
+  samples[indice] = inputValue;// Adicionar a nova amostra à matriz
+  indice = (indice + 1) % numSamples;// Atualizar o índice para a próxima amostra
+  int sum = 0; // Calcular a média das amostras
+  for (int i = 0; i < numSamples; i++) {
+    sum += samples[i];
+  }
+
+  return sum / numSamples;
+}
+
+
 
 void Task1(void * params){
     while(1){
-      count++;
-      int value = adc1_get_raw(ADC1_CHANNEL_4);
-        float sum = 0;
-        for (int i = 0; i < 10; i++) {
-           int value = adc1_get_raw(ADC1_CHANNEL_4);
-            float vout = value*(3.6/4095);
-            sum = vout;
-        } 
-        char str[8];
-        // char min[8];
-        //sprintf(str, "%.2f", sum);
-        // sprintf(min, "%d", count);
-        int valuee = adc1_get_raw(ADC1_CHANNEL_4);
-            float voutt = (valuee / 4095.0) * 3.06 * (150000 + 750000) / 750000; //valuee*(3.06/4095);
-        //Serial.println(str);
-        valores[bufferSize++] = voutt;
+      int value = 0;
+      int filteredValue = 0;
+      
+      for (int i = 0; i < 20; i++) {
+        value = adc1_get_raw(ADC1_CHANNEL_7);
+        filteredValue = FilterMovingAverage(value);
+      } 
+      
+      float vout = (filteredValue / 4095.0) * 3.27 * (150000 + 750000) / 750000;
+
+      // Serial.print("Leitura: ");
+      // Serial.print(value);
+      // Serial.print(" | ");
+      // Serial.print(filteredValue);
+      // Serial.print(" -> ");
+      // Serial.println(vout);
+
+      valores[bufferSize++] = vout;
 
 
-        //300000
-        vTaskDelay (30*60000 / portTICK_PERIOD_MS);
+
+        //30*60000
+        vTaskDelay ( 2000/ portTICK_PERIOD_MS);
 
     }
 }
@@ -73,19 +90,30 @@ void setup() {
     html += "<h1>Data Logger RAMWAY</h1>";
 
     for (int i = 0; i < bufferSize; i++) {
-      //int idx = (alx + i) % bufferSize;
       html += "<br>" + String(valores[i]);
     }
-    html += "<script>setTimeout(function(){ location.reload(); }, 10000);</script>";
+    html += "<script>setTimeout(function(){ location.reload(); }, 2000);</script>";
     html += "</body></html>";
     request->send(200, "text/html", html);
   });
-  // Servidor começa à ouvir os clientes
   server.begin();
+
   
+   adc1_config_width(ADC_WIDTH_BIT_12);
+   adc1_config_channel_atten(ADC1_CHANNEL_7, ADC_ATTEN_DB_11);
+    
+    gpio_config_t io_conf = {0};
+    io_conf.pin_bit_mask = (1ULL << GPIO_NUM_35);
+    io_conf.mode = GPIO_MODE_INPUT;
+    io_conf.pull_up_en = GPIO_PULLUP_DISABLE;
+    io_conf.pull_down_en = GPIO_PULLDOWN_ENABLE;
+    gpio_config(&io_conf);
+
+  vTaskDelay( 2000/ portTICK_PERIOD_MS);
+  Serial.println("");
+  Serial.println(WiFi.localIP());
   xTaskCreate(&Task1,"Leitura de tensao",2048, NULL,5,NULL);
-  adc1_config_width(ADC_WIDTH_BIT_12);
-  adc1_config_channel_atten(ADC1_CHANNEL_4, ADC_ATTEN_DB_0); ////porta analógica utilizada é a ADC1_CHANNEL_4, que está conectada ao pino 32 do ESP32
+  
 
 }
 
@@ -94,5 +122,5 @@ void loop() {
   // int novoValor = random(100); // Substitua por sua lógica para obter o novo valor
   // valores[alx] = novoValor;
   // alx = (alx + 1) % bufferSize;
-  delay(10000);
+  delay(10);
 }
